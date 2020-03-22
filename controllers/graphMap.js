6edacdc7df6ae837fda1f95135b179b0dede4a82
PeterGoedeke <peterbgoedeke@gmail.com>
2020-03-23 01:10:48 +1300

    Pathfinding implemented

--- controllers/graphMap.js
+++ controllers/graphMap.js
@@ -133,6 +133,92 @@ const graphMapProto = (function () {
                 return latLng
             }
             return getNearestNodeFromList(latLng, this.nodes)
+        },
+        /**
+         * Returns an array of latLng keys representing the fastest path between the two coordinate pairs provided.
+         * Uses the A* algorithm to calculate the fastest path.
+         * If the starting and ending coordinates do not exist in the graphMap, then find the nearest existing node to use instead.
+         * If the starting and edning coordinates represent the same node then warn and do nothing.
+         * If only one coordinate pair is given then warn and do nothing.
+         * If no connection exist between the two coordinate pairs then warn; if this happens it is the fault of the user's graphMap design.
+         * @param {any} startCoords The starting coordinate pair
+         * @param {any} endCoords The ending coordinate pair
+         * @returns {String[]} The fastest path between the starting and ending pairs
+         */
+        fastestRouteBetween(startCoords, endCoords) {
+            if(!startCoords || !endCoords) {
+                warn(`Pathfinding could not be undertaken as ${startCoords} or ${endCoords} does not exist.`)
+                return
+            }
+            // if the coordinates given are not node coordinates, then find the nearest nodes to the coordinates to use instead
+            startCoords = toKey(startCoords)
+            endCoords = toKey(endCoords)
+            if(!this.nodes[startCoords]) startCoords = this.getNearestNode(startCoords)
+            if(!this.nodes[endCoords]) startCoords = this.getNearestNode(endCoords)
+
+            if(startCoords == endCoords) {
+                warn(`Pathfinding could not be undertaken as the start and end location are the same: ${startCoords}.`)
+                return
+            }
+            // open and closed lists (implemented as "maps") for the A* algorithm
+            let openLength = 0 // more efficient to keep tabs than run Object.keys
+            const open = {
+                [startCoords]: createPath(0, distanceBetween(startCoords, endCoords))
+            }
+            const closed = {}
+            
+            while(openLength > 0) {
+                // remove the lowest f cost node from open and add it to closed
+                const current = retrieveLowestFCostNode(open)
+                closed[current.coords] = current.path
+                // if the current node is the end node, the algorithm has finished
+                if(current.coords === endCoords) {
+                    return current.path.parent.concat(endCoords)
+                }
+                // for each neighbour of the current node
+                for(const neighbourCoords in this.nodes[current.coords].connections) {
+                    if(closed[neighbourCoords]) continue // skip the neighbour if it is in the closed list
+                    // if the new path to the neighbour is shorter or the neighbour is not in open...
+                    const gCostToNeighbour = current.path.gCost + distanceBetween(current.coords, neighbourCoords)
+                    if(!open[neighbourCoords] || gCostToNeighbour < open[neighbourCoords].path.gCost) {
+                        // make sure the neighbour is in the open list
+                        const neighbourPath = open[neighbourCoords] ||
+                            createPath(gCostToNeighbour, distanceBetween(neighbourCoords, endCoords))
+                        // ...then set the f_cost of the neighbour and the parent of the neighbour to the current node
+                        neighbourPath.parent = current.path.parent && current.path.parent.concat(current.coords) || [current.coords]
+                        open[neighbourCoords] = neighbourPath
+                    }
+                }
+            }
+            // if there is no possible connection between nodes, return null. If this happens then the flaw is with the user's design
+            // of the graphMap, not the algorithm
+            warn(`No connection exists between ${startCoords} and ${endCoords}.`)
+            return null
+
+            // helper function used to extract (e.g. remove from the list and return) the lowest F cost node
+            function retrieveLowestFCostNode(map) {
+                let lowestSoFar = null
+                for(const coords in map) {
+                    const newFCostLower = lowestSoFar && map[coords].fCost < lowestSoFar.path.fCost
+                    const hCostBreaksTie = lowestSoFar && map[coords].fCost == lowestSoFar.path.fCost && map[coords].hCost < lowestSoFar.path.hCost
+
+                    if(!lowestSoFar || newFCostLower || hCostBreaksTie) lowestSoFar = {
+                        coords: coords,
+                        path: map[coords]
+                    }
+                }
+                delete map[lowestSoFar.coords]
+                openLength --
+                return lowestSoFar
+            }
+            // helper function to create the path data object used to store the costs of the route
+            function createPath(gCost, hCost) {
+                openLength++
+                return {
+                    gCost, hCost,
+                    get fCost() { return this.gCost + this.hCost }
+                }
+            }
         }
     }
     return graphMapProto
@@ -149,16 +235,22 @@ function createGraphMap() {
 }
 
 const test = createGraphMap()
-test.addVertex([1, 1], {letter: 'A'})
-test.addVertex('1,2', {letter: 'B'})
-test.addVertex([1, 3], {letter: 'C'})
-test.addVertex([1, 4], {letter: 'D'})
-test.addVertex([1, 5], {letter: 'E'})
-test.addEdge([1, 1], [1, 4])
-test.updateVertexProperties([1, 1], {letter: 'J'})
+test.addVertex([10, 0], {letter: 'A'})
+test.addVertex([10, 20], {letter: 'B'})
+test.addVertex([0, 0], {letter: 'C'})
+test.addVertex([0, 20], {letter: 'D'})
+test.addVertex([5, 10], {letter: 'E'})
+// test.addVertex([1, 5], {letter: 'E'})
+test.addEdge([10, 0], [10, 20])
+test.addEdge([10, 20], [0, 20])
+test.addEdge([0, 20], [0, 0])
+test.addEdge([10, 0], [0, 0])
+test.addEdge([10, 0], [5, 10])
+test.addEdge([0, 20], [5, 10])
+// test.updateVertexProperties([1, 1], {letter: 'J'})
 // test.deleteVertex([1, 4])
 console.log(JSON.stringify(test, null, 4))
 
-console.log(test.getNearestNode([1, 5]))
+console.log(test.fastestRouteBetween([0, 0], [5, 10]))
 
 module.exports = createGraphMap()
