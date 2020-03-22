3c4bada1990cc8a826b9b8ace7a88f1b251c58ab
PeterGoedeke <peterbgoedeke@gmail.com>
2020-03-23 12:27:37 +1300

    Added db

---
+++ models/db.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const warn = require('../warn.js')
+
+const dbURI = 'mongodb://localhost/map-routes'
+mongoose.connect(dbURI, { useNewUrlParser: true })
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`)
+})
+mongoose.connection.on('error', err => {
+    warn(`Mongoose connection error: ${err}`)
+})
+mongoose.connection.on('disconnected', () => {
+    warn(`Mongoose disconnected`)
+})
+
+// function to close the mongoose connection
+function gracefulShutdown(msg, callback) {
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`)
+        callback()
+    })
+}
+
+// nodemon shutdown signal
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2')
+    })
+})
+// general application shutdown signal
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0)
+    })
+})
+// heroku shutdown signal
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0)
+    })
+})
