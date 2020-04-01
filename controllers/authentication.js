3a9335b55438374dccd3c8f6365d9d071709b6d3
PeterGoedeke <peterbgoedeke@gmail.com>
2020-04-01 21:30:53 +1300

    Authentication work

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -18,6 +18,24 @@ function register(req, res) {
     })
 }
 
+function login(req, res) {
+    if(!req.body.username || !req.body.password) {
+        return res.status(400).json('All fields required')
+    }
+    passport.authenticate('local', (err, user, info) => {
+        let token
+        if(err) {
+            return res.status(404).json(err)
+        }
+        if(user) {
+            token = user.generateJwt()
+            res.status(200).json({token})
+        }
+        res.status(401).json(info)
+    })(req, res)
+}
+
+
 module.exports = {
     register,
     login
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router()
 const controllers = require('../controllers/static-pages')
 
 const databaseAPI = require('../controllers/database_api')
-const auth = require('../controllers/authentication')
+const authCtrl = require('../controllers/authentication')
+
+const jwt = require('express-jwt')
+const auth = jwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: 'payload'
+})
 
 /* GET home page. */
 router.get('/', controllers.institutionList)
@@ -11,23 +17,26 @@ router.get('/', controllers.institutionList)
 router.get('/about', function(req, res, next) {
     res.render('about', { title: 'Express' })
 })
-router.get('/opensource', function(req, res, next) {
-    res.render('openSource', { title: 'Express' })
-})
+// 366
+router.route('/opensource')
+    .get(auth, function(req, res, next) {
+        res.render('openSource', { title: 'Express' })
+    })
 router.route('/register')
     .get(function(req, res, next) {
         res.render('registerInstitution', { title: 'Express' })
     })
-    .post(auth.register)
+    .post(authCtrl.register)
 
 router.route('/admin')
     .get(function(req, res, next) {
         res.render('adminLogin', { title: 'Express' })
     })
-    .post(auth.login)
+    .post(authCtrl.login)
     
 router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Express' })
 })
+router.get('/login', authCtrl.login)
 
 module.exports = router
